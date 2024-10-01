10528916571be342b3b232b6ce4570474360baa5
Harrison Wells <aemre7478@gmail.com>
2024-10-01 06:41:43 +0300

    Added Settings - Some Bug Fix

---
+++ multronfileguardian11/Form3.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace multronfileguardian
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rjButton8 = new RJCodeAdvance.RJControls.RJButton();
+            this.rjButton7 = new RJCodeAdvance.RJControls.RJButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.showpassword0 = new System.Windows.Forms.RadioButton();
+            this.showpassword1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dfafterencrypted1 = new System.Windows.Forms.RadioButton();
+            this.dfafterencrypted0 = new System.Windows.Forms.RadioButton();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.rjButton8);
+            this.panel2.Controls.Add(this.rjButton7);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(984, 50);
+            this.panel2.TabIndex = 24;
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label5.Location = new System.Drawing.Point(9, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(282, 22);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Multron File Guardian Settings";
+            // 
+            // rjButton8
+            // 
+            this.rjButton8.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.rjButton8.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.rjButton8.BorderColor = System.Drawing.Color.ForestGreen;
+            this.rjButton8.BorderRadius = 0;
+            this.rjButton8.BorderSize = 0;
+            this.rjButton8.FlatAppearance.BorderSize = 0;
+            this.rjButton8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjButton8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rjButton8.ForeColor = System.Drawing.Color.Black;
+            this.rjButton8.Location = new System.Drawing.Point(835, 0);
+            this.rjButton8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rjButton8.Name = "rjButton8";
+            this.rjButton8.Size = new System.Drawing.Size(65, 50);
+            this.rjButton8.TabIndex = 24;
+            this.rjButton8.Text = "-";
+            this.rjButton8.TextColor = System.Drawing.Color.Black;
+            this.rjButton8.UseVisualStyleBackColor = false;
+            this.rjButton8.Click += new System.EventHandler(this.rjButton8_Click);
+            // 
+            // rjButton7
+            // 
+            this.rjButton7.BackColor = System.Drawing.Color.DarkRed;
+            this.rjButton7.BackgroundColor = System.Drawing.Color.DarkRed;
+            this.rjButton7.BorderColor = System.Drawing.Color.ForestGreen;
+            this.rjButton7.BorderRadius = 0;
+            this.rjButton7.BorderSize = 0;
+            this.rjButton7.FlatAppearance.BorderSize = 0;
+            this.rjButton7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjButton7.Font = new System.Drawing.Font("Microsoft JhengHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rjButton7.ForeColor = System.Drawing.Color.White;
+            this.rjButton7.Location = new System.Drawing.Point(919, 0);
+            this.rjButton7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rjButton7.Name = "rjButton7";
+            this.rjButton7.Size = new System.Drawing.Size(65, 50);
+            this.rjButton7.TabIndex = 23;
+            this.rjButton7.Text = "X";
+            this.rjButton7.TextColor = System.Drawing.Color.White;
+            this.rjButton7.UseVisualStyleBackColor = false;
+            this.rjButton7.Click += new System.EventHandler(this.rjButton7_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(0, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(984, 485);
+            this.panel1.TabIndex = 25;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.showpassword0);
+            this.groupBox1.Controls.Add(this.showpassword1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(251, 96);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Password Settings";
+            // 
+            // showpassword0
+            // 
+            this.showpassword0.AutoSize = true;
+            this.showpassword0.Location = new System.Drawing.Point(6, 56);
+            this.showpassword0.Name = "showpassword0";
+            this.showpassword0.Size = new System.Drawing.Size(209, 24);
+            this.showpassword0.TabIndex = 1;
+            this.showpassword0.TabStop = true;
+            this.showpassword0.Text = "Dont Show Password";
+            this.showpassword0.UseVisualStyleBackColor = true;
+            this.showpassword0.Click += new System.EventHandler(this.showpassword0_Click);
+            // 
+            // showpassword1
+            // 
+            this.showpassword1.AutoSize = true;
+            this.showpassword1.Checked = true;
+            this.showpassword1.Location = new System.Drawing.Point(6, 26);
+            this.showpassword1.Name = "showpassword1";
+            this.showpassword1.Size = new System.Drawing.Size(163, 24);
+            this.showpassword1.TabIndex = 0;
+            this.showpassword1.TabStop = true;
+            this.showpassword1.Text = "Show Password";
+            this.showpassword1.UseVisualStyleBackColor = true;
+            this.showpassword1.Click += new System.EventHandler(this.showpassword1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dfafterencrypted0);
+            this.groupBox2.Controls.Add(this.dfafterencrypted1);
+            this.groupBox2.Location = new System.Drawing.Point(269, 5);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(411, 96);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Delete File After Encrypted/Decrypted";
+            // 
+            // dfafterencrypted1
+            // 
+            this.dfafterencrypted1.AutoSize = true;
+            this.dfafterencrypted1.Location = new System.Drawing.Point(6, 26);
+            this.dfafterencrypted1.Name = "dfafterencrypted1";
+            this.dfafterencrypted1.Size = new System.Drawing.Size(61, 24);
+            this.dfafterencrypted1.TabIndex = 0;
+            this.dfafterencrypted1.Text = "Yes";
+            this.dfafterencrypted1.UseVisualStyleBackColor = true;
+            this.dfafterencrypted1.Click += new System.EventHandler(this.dfafterencrypted1_Click);
+            // 
+            // dfafterencrypted0
+            // 
+            this.dfafterencrypted0.AutoSize = true;
+            this.dfafterencrypted0.Checked = true;
+            this.dfafterencrypted0.Location = new System.Drawing.Point(6, 56);
+            this.dfafterencrypted0.Name = "dfafterencrypted0";
+            this.dfafterencrypted0.Size = new System.Drawing.Size(53, 24);
+            this.dfafterencrypted0.TabIndex = 1;
+            this.dfafterencrypted0.TabStop = true;
+            this.dfafterencrypted0.Text = "No";
+            this.dfafterencrypted0.UseVisualStyleBackColor = true;
+            this.dfafterencrypted0.Click += new System.EventHandler(this.dfafterencrypted0_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(984, 535);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MFG_Settings";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private RJCodeAdvance.RJControls.RJButton rjButton8;
+        private RJCodeAdvance.RJControls.RJButton rjButton7;
+        public System.Windows.Forms.RadioButton showpassword1;
+        public System.Windows.Forms.RadioButton showpassword0;
+        public System.Windows.Forms.GroupBox groupBox1;
+        public System.Windows.Forms.Panel panel1;
+        public System.Windows.Forms.GroupBox groupBox2;
+        public System.Windows.Forms.RadioButton dfafterencrypted0;
+        public System.Windows.Forms.RadioButton dfafterencrypted1;
+    }
+}
