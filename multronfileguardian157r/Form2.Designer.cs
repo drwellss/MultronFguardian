04ba14c800e8e7d1e1466ba7b5feb3c78180d45c
drwellss <aemre7478@gmail.com>
2025-11-22 05:37:47 +0300

    Shredder added

---
+++ multronfileguardian157r/Form2.Designer.cs
@@ -0,0 +1,289 @@
+﻿
+namespace multronfileguardian
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label3;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rjButton4 = new RJCodeAdvance.RJControls.RJButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rjButton8 = new RJCodeAdvance.RJControls.RJButton();
+            this.rjButton7 = new RJCodeAdvance.RJControls.RJButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            label3 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label3.ForeColor = System.Drawing.Color.DarkRed;
+            label3.Location = new System.Drawing.Point(321, 422);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(302, 22);
+            label3.TabIndex = 7;
+            label3.Text = "Store your Private Key Securely!";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(24, 71);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(441, 329);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(517, 71);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(441, 329);
+            this.textBox2.TabIndex = 3;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.ForeColor = System.Drawing.Color.Black;
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Location = new System.Drawing.Point(7, 476);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(30, 31);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "?";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(177, 402);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 22);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Public Key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(679, 402);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 22);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Private Key";
+            // 
+            // rjButton4
+            // 
+            this.rjButton4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(90)))), ((int)(((byte)(71)))));
+            this.rjButton4.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(90)))), ((int)(((byte)(71)))));
+            this.rjButton4.BorderColor = System.Drawing.Color.ForestGreen;
+            this.rjButton4.BorderRadius = 5;
+            this.rjButton4.BorderSize = 0;
+            this.rjButton4.FlatAppearance.BorderSize = 0;
+            this.rjButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjButton4.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rjButton4.ForeColor = System.Drawing.Color.White;
+            this.rjButton4.Location = new System.Drawing.Point(379, 457);
+            this.rjButton4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rjButton4.Name = "rjButton4";
+            this.rjButton4.Size = new System.Drawing.Size(225, 50);
+            this.rjButton4.TabIndex = 20;
+            this.rjButton4.Text = "Generate RSA Key";
+            this.rjButton4.TextColor = System.Drawing.Color.White;
+            this.rjButton4.UseVisualStyleBackColor = false;
+            this.rjButton4.Click += new System.EventHandler(this.rjButton4_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(90)))), ((int)(((byte)(71)))));
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.rjButton8);
+            this.panel2.Controls.Add(this.rjButton7);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(984, 50);
+            this.panel2.TabIndex = 23;
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label5.Location = new System.Drawing.Point(9, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(258, 22);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Multron RSA Key Generator";
+            // 
+            // rjButton8
+            // 
+            this.rjButton8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(90)))), ((int)(((byte)(71)))));
+            this.rjButton8.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(90)))), ((int)(((byte)(71)))));
+            this.rjButton8.BorderColor = System.Drawing.Color.ForestGreen;
+            this.rjButton8.BorderRadius = 0;
+            this.rjButton8.BorderSize = 0;
+            this.rjButton8.FlatAppearance.BorderSize = 0;
+            this.rjButton8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjButton8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rjButton8.ForeColor = System.Drawing.Color.White;
+            this.rjButton8.Location = new System.Drawing.Point(846, 0);
+            this.rjButton8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rjButton8.Name = "rjButton8";
+            this.rjButton8.Size = new System.Drawing.Size(65, 50);
+            this.rjButton8.TabIndex = 24;
+            this.rjButton8.Text = "-";
+            this.rjButton8.TextColor = System.Drawing.Color.White;
+            this.rjButton8.UseVisualStyleBackColor = false;
+            this.rjButton8.Click += new System.EventHandler(this.rjButton8_Click);
+            // 
+            // rjButton7
+            // 
+            this.rjButton7.BackColor = System.Drawing.Color.DarkRed;
+            this.rjButton7.BackgroundColor = System.Drawing.Color.DarkRed;
+            this.rjButton7.BorderColor = System.Drawing.Color.ForestGreen;
+            this.rjButton7.BorderRadius = 0;
+            this.rjButton7.BorderSize = 0;
+            this.rjButton7.FlatAppearance.BorderSize = 0;
+            this.rjButton7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjButton7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.rjButton7.ForeColor = System.Drawing.Color.White;
+            this.rjButton7.Location = new System.Drawing.Point(919, 0);
+            this.rjButton7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rjButton7.Name = "rjButton7";
+            this.rjButton7.Size = new System.Drawing.Size(65, 50);
+            this.rjButton7.TabIndex = 23;
+            this.rjButton7.Text = "X";
+            this.rjButton7.TextColor = System.Drawing.Color.White;
+            this.rjButton7.UseVisualStyleBackColor = false;
+            this.rjButton7.Click += new System.EventHandler(this.rjButton7_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(728, 452);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(157, 22);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "RSA Key Length";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "2048",
+            "3072",
+            "4096",
+            "8192",
+            "16384"});
+            this.comboBox2.Location = new System.Drawing.Point(732, 476);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(251, 30);
+            this.comboBox2.TabIndex = 27;
+            this.comboBox2.Tag = "";
+            this.comboBox2.SelectedValueChanged += new System.EventHandler(this.comboBox2_SelectedValueChanged);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(984, 511);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.rjButton4);
+            this.Controls.Add(label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(984, 511);
+            this.MinimumSize = new System.Drawing.Size(984, 511);
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Multron RSA Key Generator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.Shown += new System.EventHandler(this.Form2_Shown);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private RJCodeAdvance.RJControls.RJButton rjButton4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private RJCodeAdvance.RJControls.RJButton rjButton8;
+        private RJCodeAdvance.RJControls.RJButton rjButton7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
---
+++ multronfileguardian157r/Form4.cs
@@ -0,0 +1,171 @@
+﻿using RJCodeAdvance.RJControls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace multronfileguardian
+{
+    public partial class Form4 : Form
+    {
+        string mfgkeys = Application.StartupPath + "\\mfgsettings\\" + "publickeys.txt";
+        Form1 frm1;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                const int CS_DROPSHADOW = 0x20000;
+                CreateParams cp = base.CreateParams;
+                cp.ClassStyle |= CS_DROPSHADOW;
+                return cp;
+            }
+        }
+
+        public Form4(Form1 nform)
+        {
+            InitializeComponent();
+            frm1 = nform;
+        }
+        private void darktheme()
+        {
+            panel2.BackColor = Color.FromArgb(40, 40, 40);
+            this.BackColor = Color.FromArgb(23, 23, 23);
+            rjButton8.BackColor = panel2.BackColor;
+            foreach (Control yazi in this.Controls)
+            {
+                if (yazi.Name.Contains("rjButton"))
+                {
+
+                    yazi.BackColor = Color.FromArgb(43, 40, 40);
+                    yazi.ForeColor = Color.WhiteSmoke;
+
+                }
+                if (yazi.Name.Contains("label"))
+                {
+                    if (yazi.Name != "label3")
+                    {
+                        yazi.ForeColor = Color.WhiteSmoke;
+                    }
+                    else
+                    {
+                        yazi.ForeColor = Color.Red;
+                    }
+                }
+                if (yazi.Name.Contains("linkLabel"))
+                {
+                    LinkLabel label = (LinkLabel)yazi;
+                    label.LinkColor = Color.WhiteSmoke;
+                }
+                if (yazi.Name.Contains("textBox"))
+                {
+                    yazi.BackColor = Color.FromArgb(30, 30, 30);
+                    yazi.ForeColor = Color.WhiteSmoke;
+                }
+                if (yazi.Name.Contains("listBox"))
+                {
+                    yazi.BackColor = Color.FromArgb(30, 30, 30);
+                    yazi.ForeColor = Color.WhiteSmoke;
+                }
+            }
+        }
+        private void Form4_Load(object sender, EventArgs e)
+        {
+
+            if (frm1.formuc.theme1.Checked == true)
+            {
+                System.Threading.Thread threddd = new System.Threading.Thread(() => darktheme());
+                threddd.Start();
+            }
+            if (File.Exists(mfgkeys))
+            {
+                listBox1.Items.AddRange(File.ReadAllLines(mfgkeys));
+            }
+        }
+
+        private void rjButton1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text + "=" + textBox2.Text);
+        }
+
+        private void rjButton2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+
+        private void rjButton3_Click(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>();
+            foreach (string s in listBox1.Items) 
+            {
+                list.Add(s); 
+            }
+            File.WriteAllLines(mfgkeys, list.ToArray());
+        }
+
+        private void rjButton4_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string pbkey = "";
+                short keystate = 0;
+                foreach (char c in listBox1.SelectedItem.ToString())
+                {
+                    if (keystate == 1)
+                    {
+                        pbkey = pbkey + c;
+                    }
+                    if (c == '=')
+                    {
+                        keystate = 1;
+                    }
+                }
+                frm1.textBox1.Text = pbkey;
+                if (frm1.formuc.ckh1.Checked == true)
+                {
+                    Close();
+                }
+            }
+            
+        }
+
+        private void panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, 0xA1, 0x2, 0);
+            }
+        }
+
+        private void rjButton7_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void rjButton8_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This area dont provide any encryption, please dont save your own symmetric key or dont save private key, Just save public keys", "Multron Key Hold", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void Form4_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            frm1.closedholdkey = 1;
+        }
+    }
+}
